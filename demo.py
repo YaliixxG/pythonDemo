cf7c5d1ffa379dec73a1b5191e11842da2409f70
YaliixxG <529008421@qq.com>
2018-09-07 17:45:07 +0800

    add:demo

--- demo.py
+++ demo.py
@@ -247,3 +247,37 @@
 # mm = int(input('输入月份：'))
 
 # print(calendar.month(yy, mm))
+
+
+# 使用递归斐波那契数列
+
+# def recur_fibo(n):
+#     """递归函数
+#     输出斐波那契数列"""
+#     if n <= 1:
+#         return n
+#     else:
+#         return(recur_fibo(n-1) + recur_fibo(n-2))
+
+
+# # 获取用户输入
+# nterms = int(input("您要输出几项? "))
+
+# # 检查输入的数字是否正确
+# if nterms <= 0:
+#     print("输入正数")
+# else:
+#     print("斐波那契数列:")
+#     for i in range(nterms):
+#         print(recur_fibo(i))
+
+
+# 文件IO
+
+with open('test.txt', 'wt') as out_file:
+    out_file.write("该文本会写入到文件中\n看到我了吧！")
+
+with open('test.txt', 'rt') as in_file:
+    text = in_file.read()
+
+print(text)
